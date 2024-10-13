dc411d9dad0fe3b65e29cdb6493db5639e55cb10
Elekss-St <elekss.st@gmail.com>
2024-10-13 23:20:44 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+from math import inf
+def divide(a,b):
+    result = 0
+    if b == 0:
+        result = inf
+    else:
+        result = a/b
+    return result
---
+++ true_math.py
@@ -0,0 +1,7 @@
+def divide(a,b):
+    result = 0
+    if b == 0:
+        result = "Ошибка"
+    else:
+        result = a/b
+    return result

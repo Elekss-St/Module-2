a647c4b78108875778b48be3adf2371aedda1b7a
Elekss-St <elekss.st@gmail.com>
2024-10-20 23:30:39 +0300

    Add files via upload

--- Module_4_1.py
+++ Module_4_1.py
@@ -1,6 +1,6 @@
 from fake_math import divide as fake_divide
 from true_math import divide as true_divide
-
+import sys
 result1 = fake_divide(69, 3)
 result2 = fake_divide(3, 0)
 result3 = true_divide(49, 7)
@@ -9,3 +9,6 @@ print(result1)
 print(result2)
 print(result3)
 print(result4)
+
+for path in sys.path:
+    print (path)
